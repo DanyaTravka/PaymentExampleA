668dc84a3cd9aa39b8ed17c0f610679a6122a2e8
rotoyouoi <rotoyouoi@yandex.ru>
2023-02-01 10:45:37 +0500

    Добавьте файлы проекта.

---
+++ View/Pages/ChartsPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaymentExampleApp.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ChartsPage.xaml
+    /// </summary>
+    public partial class ChartsPage : Page
+    {
+
+        public ChartsPage()
+        {
+            InitializeComponent();
+            ChartPayments.ChartAreas.Add(new ChartArea("Main"));
+
+            var currentSeries = new Series("Payments")
+            {
+                IsValueShownAsLabel = true
+            };
+            ChartPayments.Series.Add(currentSeries);
+
+            ComboUsers.ItemsSource = _context.Users.ToList();
+            ComboChartTypes.ItemsSource = Enum.GetValues(typeof(SeriesChartType));
+        }
+
+
+
+        private void BtnExportToExcel_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateChart(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboUsers.SelectedItem is UserControl currentUser && ComboChartTypes.SelectedItem is SeriesChartType currentType)
+            {
+                Series currentSeries = chartPayments.Series.FirstOrDefault();
+                currentSeries.ChartType = currentType;
+                currentSeries.Points.Clear();
+
+                var categoriesList = _context.Categories.ToList();
+                foreach (var category in categoriesList)
+                {
+                    currentSeries.Points.AddXY(category.name, _context.Payments.ToList().Where(p => p.users == currentUser && p.category == category).Sum(p => p.Price * p.Num))
+                }
+            }
+        }
+    }
+}
